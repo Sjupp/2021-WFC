b5575d73c421c1abf2b2b330d62e0f031548e942
Anton <a.e.lindkvist@gmail.com>
2021-05-13 12:59:14 +0200

    Initial commit

---
+++ Assets/Scripts/BasicTile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BasicTile", menuName = "", order = 1)]
+public class BasicTile : ScriptableObject
+{
+    public Sprite Sprite = null;
+
+    public List<int> NorthNeighbors = new List<int>();
+    public List<int> SouthNeighbors = new List<int>();
+    public List<int> WestNeighbors = new List<int>();
+    public List<int> EastNeighbors = new List<int>();
+}

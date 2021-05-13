a6b92adca3882ebb5c6be424ab36ec9e607df9b9
Anton <a.e.lindkvist@gmail.com>
2021-05-13 13:38:30 +0200

    Creating a grid of Cells

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    private Vector2Int _coordinates = new Vector2Int();
+    private List<BasicTile> _tiles = new List<BasicTile>();
+
+    public Vector2Int Coordinates { get => _coordinates; }
+    public List<BasicTile> Tiles { get => _tiles; }
+
+    public void Init(Vector2Int coords, List<BasicTile> tiles)
+    {
+        _coordinates = coords;
+        _tiles = tiles;
+    }
+    
+    public int GetCellEntropy()
+    {
+        int number = 0;
+        
+        foreach (BasicTile tile in _tiles)
+        {
+            number += tile.NorthNeighbors.Count;
+            number += tile.SouthNeighbors.Count;
+            number += tile.EastNeighbors.Count;
+            number += tile.WestNeighbors.Count;
+        }
+
+        return number;
+    }
+}
---
+++ Assets/Scripts/Solver.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Solver : MonoBehaviour
+{
+    [SerializeField]
+    private List<BasicTile> _tiles = new List<BasicTile>();
+    [SerializeField]
+    private Vector2Int _size = new Vector2Int();
+
+    [SerializeField]
+    private Cell _cellPrefab = null;
+
+    private List<Cell> _cells = new List<Cell>();
+
+    private void Start()
+    {
+        Initialize(_size, _tiles);
+    }
+
+    private void Initialize(Vector2Int fieldSize, List<BasicTile> tiles)
+    {
+        for (int y = 0; y < fieldSize.y; y++)
+        {
+            for (int x = 0; x < fieldSize.x; x++)
+            {
+                var obj = Instantiate(_cellPrefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 0));
+                _cells.Add(obj);
+                obj.Init(new Vector2Int(x, y), _tiles);
+            }
+        }
+    }
+
+    private Vector2Int GetMinimumEntropy()
+    {
+        List<Cell> cells = null;
+        int minEnt = 1000;
+        foreach (var cell in _cells)
+        {
+            int cellEnt = cell.GetCellEntropy();
+            if (cellEnt < minEnt)
+            {
+                minEnt = cellEnt;
+                cells.Clear();
+                cells.Add(cell);
+            }
+            else if (cellEnt == minEnt)
+            {
+                cells.Add(cell);
+            }
+        }
+
+        if (cells.Count > 1)
+            return cells[Random.Range(0, cells.Count)].Coordinates;
+        else
+            return cells[0].Coordinates;
+    }
+}
